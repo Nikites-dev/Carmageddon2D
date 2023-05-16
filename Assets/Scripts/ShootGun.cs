5ff6e4d11007ebf5fac672b4f4cbc4115c76b30d
Петров Никита <Petovn.xz@mail.ru>
2023-05-16 11:03:56 +0300

    trying add logic movement booled

---
+++ Assets/Scripts/ShootGun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class ShootGun:MonoBehaviour
+    {
+        [SerializeField] private float _moveSpeed;
+        [SerializeField] private GameObject bulletPrefab;
+        [SerializeField] private Transform carPrefab;
+        
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.V))
+            {
+                // Создание объекта пули
+                GameObject bullet = Instantiate(bulletPrefab, carPrefab.position, carPrefab.rotation);
+
+                bullet.transform.position += Vector3.right* 100 * Time.deltaTime;
+                
+                // Задание скорости пули
+                bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.forward * 6;
+                
+                
+            }
+        }
+    }
+}
