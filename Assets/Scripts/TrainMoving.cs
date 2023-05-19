c5493f896842d8e13108d31139bbffbf2067ce23
Nikites <rockstar.gg@mail.ru>
2023-05-19 13:10:19 +0300

    add train

---
+++ Assets/Scripts/TrainMoving.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class TrainMoving:MonoBehaviour
+    {
+        [SerializeField] private float _moveSpeed;
+        [SerializeField] private GameObject trainPrefab;
+        [SerializeField] private Transform pointPrefab;
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                var pos = pointPrefab.position;
+                pos.x -= 6.5f;
+                pos.y -= 3.0f;
+                
+                // Создание объекта пули
+                GameObject train = Instantiate(trainPrefab, pos, pointPrefab.rotation);
+                
+                // Задание скорости пули
+                train.GetComponent<Rigidbody2D>().velocity = train.transform.right * _moveSpeed;
+            }
+        }
+    }
+}
