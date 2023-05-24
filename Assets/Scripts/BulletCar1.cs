35cc1efcd6092ab095599ce4be47a585e1d632f1
Петров Никита <Petovn.xz@mail.ru>
2023-05-24 09:11:19 +0300

    add bullet car1 collision destroyed with coffee revearse and bullet car 2

---
+++ Assets/Scripts/BulletCar1.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class BulletCar1:MonoBehaviour
+    {
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.tag == "Bullet1")
+            {
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+}
